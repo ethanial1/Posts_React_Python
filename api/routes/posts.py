8db5ec78e58cba8525e612f4c4c515c96b374f99
ethanial1 <camiloeiz00@gmail.com>
2022-02-07 11:18:43 -0600

    ruta para guardar un post

--- api/routes/posts.py
+++ api/routes/posts.py
@@ -1,4 +1,6 @@
-from flask import Blueprint
+from flask import Blueprint, request, jsonify
+from models.posts import Posts
+from utils.db import db
 
 posts = Blueprint('posts', __name__)
 
@@ -9,7 +11,21 @@ def list():
 
 @posts.route('/create', methods=['POST'])
 def save_post():
-    return 'guardar post'
+
+    try:
+        nombre, descrip = request.json.values()
+        if not nombre or not descrip:
+            raise Exception
+        
+        nuevo_post = Posts(nombre=nombre, descrip=descrip)
+        
+        db.session.add(nuevo_post)
+        db.session.commit()
+    
+        return jsonify({"msg": "post guardado correctamente"})
+
+    except Exception as ex:
+        return jsonify({"msg": "ocurrio un error"}), 400
 
 
 @posts.route('/delete', methods=['DELETE'])
